6aabe564a39cdf3049439361adb2ce4efb10855e
syedkhasim15 <syedtahaseen123@gmail.com>
2024-08-26 19:45:41 +0530

    production is enabled

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   authApiEndPoint: 'https://api.escuelajs.co/api/v1',
   API_KEY: '68cd0494fa2e4420b71918442e9fa34d',
   firebaseConfig: {
